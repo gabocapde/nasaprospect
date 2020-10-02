6c47d2ff419a6f44e8e7b43f5d7b3927955a0f2d
Gabriel Capdevila <capdevilagabriel@gmail.com>
2020-10-02 16:09:41 +0100

    Added changes to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: {
+    main: ['./src/js/main.js'],
+    vendor: [
+      'jquery'
+    ]
   },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist/'
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+          test: /\.js$/,
+          loader: 'shebang-loader'
+      }
+    ]
+  }
 };
